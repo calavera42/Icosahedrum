0ba708f79319bac9986bb07ae10108026d8e1913
calavera42 <gregdestemione@gmail.com>
2022-10-04 18:16:52 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-let symbols = "abcdefghijklmnopqrstuvwxyz?!@#$%&*";
+let symbols = "icosahedrum";
+let fadeoutDelay = 500;
+let index = 0;
 
 function Start() {
     setInterval(function(){
-        document.getElementById('level').textContent = symbols[Math.round(Math.random() * symbols.length)];
-    }, 50);
+        document.getElementById('level').textContent = symbols[index];
+        index++;
+    }, fadeoutDelay / symbols.length);
 
-    $("body").fadeOut(500, function() {
+    $("body").fadeOut(fadeoutDelay, function() {
         window.location = "./game.html";
     });
 }
